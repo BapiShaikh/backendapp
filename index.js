7b1d53a76bbc99c6bcb5e6f32199b0850e781656
Bapi Shaikh <bapishaikh1148@gmail.com>
2025-02-11 23:35:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twiter", (req, res) => {
+  res.send("twiter.com");
+});
+
+app.get("/login", (req, res) => {
+  res.send("please login bapi");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>You become a backend developer</h2>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
